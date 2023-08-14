a80069d4435160e81fe403aff074cf31d041297d
fadymaged94 <magedfady77@gmail.com>
2023-08-14 03:51:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - A c program that prints aline to check error
+ * Description: 101-quote.c
+ * Return: always 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
