1adc77b7ca4754e037e0a6bdfa52d186e85d95a6
fadymaged94 <magedfady77@gmail.com>
2023-08-28 12:45:45 -0700

    0-memset.c

---
+++ 0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Fills a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the diserd value
+ * @n: numbers of bytes to be changed
+ *
+ * Return: changed array with new value fr n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int j = 0;
+for (; n > 0; j++)
+{
+s[j] = b;
+n--;
+}
+return (s);
+}
